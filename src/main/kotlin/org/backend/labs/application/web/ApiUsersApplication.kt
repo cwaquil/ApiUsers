db4e82f0678dc1a3dfd5908ed568a790846c227d
Laerte <laerte_256@hotmail.com>
2020-10-12 19:47:29 -0300

    update na user dao e conect com banco

--- src/main/kotlin/org/backend/labs/application/web/ApiUsersApplication.kt
+++ src/main/kotlin/org/backend/labs/application/web/ApiUsersApplication.kt
@@ -1,6 +1,15 @@
 package org.backend.labs.application.web
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
+import io.javalin.apibuilder.ApiBuilder.path
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+
 
 class ApiUsersApplication {
 
@@ -8,8 +17,24 @@ class ApiUsersApplication {
 
     fun appStart() {
         app = Javalin.create().start(7000)
+        connectDB()
     }
+
+
+
+    private fun connectDB() {
+        val config = HikariConfig()
+        config.jdbcUrl = "jdbc:postgresql://localhost:5432/postgres"
+        config.username = "postgres"
+        config.password = "123456"
+
+        val dataSource = HikariDataSource(config)
+
+        Database.connect(dataSource)
+    }
+
 }
+
 
 fun main() {
     ApiUsersApplication().appStart()
--- src/main/kotlin/org/backend/labs/domain/repos/UserDAO.kt
+++ src/main/kotlin/org/backend/labs/domain/repos/UserDAO.kt
@@ -1,9 +1,15 @@
 package org.backend.labs.domain.repos
 
 import org.backend.labs.domain.User
+import org.backend.labs.domain.UserTable
+import org.jetbrains.exposed.sql.ResultRow
 
 class UserDAO() {
     fun save(nome: String,email: String): User {
         return User(nome, email)
     }
 }
+private fun ResultRow.toUser() = User(
+        name = this[UserTable.name],
+        email = this[UserTable.email]
+)
