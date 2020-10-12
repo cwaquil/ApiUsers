d193bec73403510f87bc314b1acab70642de0e61
vinicius cassin <viniciuscassin@viniciuss-MacBook-Pro.local>
2020-10-12 17:37:50 -0300

    WIP - Adicionando imports de teste

---
+++ src/test/kotlin/unit/saveServiceTest.kt
@@ -0,0 +1,7 @@
+package unit
+
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
